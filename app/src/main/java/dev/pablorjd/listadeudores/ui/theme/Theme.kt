8adfab8ed15428f61a0de0457f50e4a55155a5fc
pablorjd1 <pablo.jimenez2221@gmail.com>
2024-07-21 22:13:13 -0400

    Se configura el theme

--- app/src/main/java/dev/pablorjd/listadeudores/ui/theme/Theme.kt
+++ app/src/main/java/dev/pablorjd/listadeudores/ui/theme/Theme.kt
@@ -8,19 +8,28 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = primaryWhite,
+    secondary = gray,
+    tertiary = lightGray,
+    surface = primaryBlack,
+    background = primaryBlack
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = primaryBlack,
+    secondary = gray,
+    tertiary = lightGray,
+    surface = primaryWhite,
+    background = primaryWhite
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -33,11 +42,23 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+private val LocalDimens = staticCompositionLocalOf { DefaultDimens }
+@Composable
+fun ProvideDimens(
+    dimens: Dimens,
+    content: @Composable () -> Unit
+) {
+    val dimensionSet = remember {
+        dimens
+    }
+    CompositionLocalProvider (LocalDimens provides dimensionSet, content = content)
+}
 @Composable
 fun ListaDeudoresTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
+    windowSize: WindowWidthSizeClass = WindowWidthSizeClass.Compact,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -49,10 +70,19 @@ fun ListaDeudoresTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+    val dimensions = if(windowSize > WindowWidthSizeClass.Compact) TabletDimens else DefaultDimens
+    ProvideDimens(dimens = dimensions) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
+}
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+object ListaDeudoresTheme {
+    val dimens: Dimens
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalDimens.current
 }
--- app/src/main/java/dev/pablorjd/listadeudores/ui/theme/Type.kt
+++ app/src/main/java/dev/pablorjd/listadeudores/ui/theme/Type.kt
@@ -2,33 +2,50 @@ package dev.pablorjd.listadeudores.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import dev.pablorjd.listadeudores.R
 
 // Set of Material typography styles to start with
+val PoppinsFont = FontFamily(
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_medium, FontWeight.SemiBold),
+    Font(R.font.poppins_light,FontWeight.Light),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+)
+// Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
+    displayLarge = TextStyle(
+        fontFamily = PoppinsFont,
+        fontWeight = FontWeight.Bold,
+        fontSize = 88.sp,
+        lineHeight = 0.sp,
+        letterSpacing = 0.5.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = PoppinsFont,
+        fontWeight = FontWeight.Bold,
+        fontSize = 56.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+    bodyLarge = TextStyle(
+        fontFamily = PoppinsFont,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.5.sp,
+        textAlign = TextAlign.Center
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = PoppinsFont,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        letterSpacing = 0.5.sp,
+        textAlign = TextAlign.Center
+    ),
 )
