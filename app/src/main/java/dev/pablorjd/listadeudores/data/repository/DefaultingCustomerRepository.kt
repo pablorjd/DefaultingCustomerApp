f870fd7350f087caf87cfa37670f46857bd81f16
pablorjd1 <pablo.jimenez2221@gmail.com>
2024-07-21 22:18:39 -0400

    Se implementan metodos del dao en el repositorio transformando la data mediante el modelo del dominio

---
+++ app/src/main/java/dev/pablorjd/listadeudores/data/repository/DefaultingCustomerRepository.kt
@@ -0,0 +1,31 @@
+package dev.pablorjd.listadeudores.data.repository
+
+import dev.pablorjd.listadeudores.data.dao.DefaultingCustomerDao
+import dev.pablorjd.listadeudores.domain.model.DefaultingCustomerModel
+import dev.pablorjd.listadeudores.domain.model.toCustomerModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class DefaultingCustomerRepository @Inject constructor(private val defaultingCustomerDao: DefaultingCustomerDao){
+
+    val allDefaultingCustomers: Flow<List<DefaultingCustomerModel>> = defaultingCustomerDao.getAllDefaultingCustomer().map { item ->
+        item.map {
+            DefaultingCustomerModel(it.id, it.name, it.detail, it.amount)
+        }
+    }
+    suspend fun add(defaultingCustomerModel: DefaultingCustomerModel) {
+        defaultingCustomerDao.insertDefaultingCustomer(defaultingCustomerModel.toCustomerModel())
+    }
+    suspend fun update(defaultingCustomerModel: DefaultingCustomerModel) {
+        defaultingCustomerDao.insertDefaultingCustomer(defaultingCustomerModel.toCustomerModel())
+    }
+    suspend fun delete(defaultingCustomerModel: DefaultingCustomerModel) {
+        defaultingCustomerDao.deleteDefaultingCustomer(defaultingCustomerModel.toCustomerModel())
+    }
+
+
+
+}
